2c373e0b599a052d9b8400191b9a008e872bc94e
KevinOchoa8 <thirdbuckle6409@gmail.com>
2025-11-11 20:03:37 -0500

    Add files via upload

---
+++ prjPoliRetos/PoliRetos/src/App.java
@@ -0,0 +1,95 @@
+import java.util.Random;
+import java.util.Scanner;
+
+import SerieNumerico1.SN10;
+import SerieNumerico1.SN9;
+import Figura3.F13;
+import Figura3.F14;
+import Figura3.F15;
+import CadenaCaracter4.CC9;
+import Array5.A5;
+import Loading6.L11;
+import Loading6.L12;
+import Recursion7.R5;
+import GrafoAutomata8.GA4;
+import GrafoAutomata8.GA5;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scanner = new Scanner(System.in);
+        Random random   = new Random();
+
+        System.out.println("==========================================POLIRETOS===========================================");
+        
+        System.out.println("Grupo #4 ");
+        System.out.println("Integrantes: Tiffany Montaluisa, Ariel Montufar, Kevin Ochoa, Erik Pilatuña, Justin Pincay");
+        System.out.println("==============================================================================================\n\n");
+
+        //POLIRETOS =============================
+
+        // SN9.SerieNumericoFor(scanner);
+        // SN9.SerieNumericoWhile(scanner);
+        // SN9.SerieNumericoDoWhile(scanner);
+
+        // SN10.SerieNumericoFor(scanner);
+        // SN10.SerieNumericoWhile(scanner);
+        // SN10.SerieNumericoDoWhile(scanner);
+
+
+
+        // SC8.SerieCacacterFor(scanner);
+        // SC8.SerieCacacterWhile(scanner);
+        // SC8.SerieCaracterDoWhile(scanner);
+
+        // SC9.SerieCaracterFor(scanner);
+        // SC9.SerieCaracterWhile(scanner);
+        // SC9.SerieCaracterDoWhile(scanner);
+
+
+
+        // F13.FiguraFor(scanner);
+        // F13.FiguraWhile(scanner);
+        // F13.FiguraDoWhile(scanner);
+
+        // F14.FiguraFor(scanner);
+        // F14.FiguraWhile(scanner);
+        // F14.FiguraDoWhile(scanner);
+
+        // F15.FiguraFor(scanner);
+        // F15.FiguraWhile(scanner);
+        // F15.FiguraDoWhile(scanner);
+
+     
+
+        // CC9.CadenaCaracterFor(scanner);
+        // CC9.CadenaCaracterWhile(scanner);
+        // CC9.CadenaCaracterDoWhile(scanner);
+
+
+
+        // A5.ArrayFor(scanner, random);
+        // A5.ArrayWhile(scanner, random);
+        // A5.ArrayDoWhile(scanner, random);
+
+     
+
+        // L11.LoadingFor(scanner, random);
+        // L11.LoaginWhile(scanner, random);
+        // L11.LoadingDoWhile(scanner, random);
+
+        // L12.LoadingFor(scanner);
+        // L12.LoadingWhile(scanner);
+        // L12.LoadingDoWhile(scanner);
+
+  
+
+        // R5.Recursion(scanner);
+
+  
+
+        // GA4.numeroDecimal(scanner);
+        // GA5.validarVariable(scanner);
+
+    }
+}
---
+++ prjPoliRetos/PoliRetos/src/Figura3/F13.java
@@ -0,0 +1,63 @@
+package Figura3;
+
+import java.util.Scanner;
+
+public class F13 {
+
+    public static void FiguraFor(Scanner scanner){
+        
+        System.out.print("Ingrese el número de filas que desea del triángulo de números: ");
+        
+        int numFila = scanner.nextInt();
+        
+        for (int i = 1; i <= numFila; i++) {
+            for (int j = 1; j <= i; j++) {
+                int numMostrar = ((j - 1) % 9) + 1;  // 1-9
+                System.out.print(numMostrar);
+            }
+            System.out.println();
+        }
+        System.out.print("\n================================================================\n");
+
+    }
+
+    public static void FiguraWhile(Scanner scanner){
+        
+        System.out.print("Ingrese el número de filas que desea del triángulo de números: ");
+        
+        int numFila = scanner.nextInt();
+        
+        int i = 1;
+        while (i <= numFila){
+            int j = 1;
+            while (j <= i){
+                int numMostrar = ((j - 1) % 9) + 1;  
+                System.out.print(numMostrar);
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+        System.out.print("\n================================================================\n");
+    }
+
+    public static void FiguraDoWhile(Scanner scanner){
+        
+        System.out.print("Ingrese el número de filas que desea del triángulo de números: ");
+        
+        int numFila = scanner.nextInt();
+        
+        int i = 1;
+        do {
+            int j = 1;
+            do {
+                int numMostrar = ((j - 1) % 9) + 1;  
+                System.out.print(numMostrar);
+                j++;
+            } while (j <= i);
+            System.out.println();
+            i++;
+        } while (i <= numFila);
+        System.out.print("\n================================================================\n");
+    }
+}
